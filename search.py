d9a15a542530efb1df5c5e14e18c2fc35d4cbb13
Ethan Vizitei <ethan.vizitei@gmail.com>
2017-08-04 10:39:38 -0700

    first depth-first-search implementation

--- search.py
+++ search.py
@@ -7,7 +7,7 @@
 # For more info, see http://inst.eecs.berkeley.edu/~cs188/sp09/pacman.html
 
 """
-In search.py, you will implement generic search algorithms which are called 
+In search.py, you will implement generic search algorithms which are called
 by Pacman agents (in searchAgents.py).
 """
 
@@ -17,20 +17,20 @@ class SearchProblem:
   """
   This class outlines the structure of a search problem, but doesn't implement
   any of the methods (in object-oriented terminology: an abstract class).
-  
+
   You do not need to change anything in this class, ever.
   """
-  
+
   def getStartState(self):
      """
-     Returns the start state for the search problem 
+     Returns the start state for the search problem
      """
      util.raiseNotDefined()
-    
+
   def isGoalState(self, state):
      """
        state: Search state
-    
+
      Returns True if and only if the state is a valid goal state
      """
      util.raiseNotDefined()
@@ -38,11 +38,11 @@ class SearchProblem:
   def getSuccessors(self, state):
      """
        state: Search state
-     
-     For a given state, this should return a list of triples, 
-     (successor, action, stepCost), where 'successor' is a 
+
+     For a given state, this should return a list of triples,
+     (successor, action, stepCost), where 'successor' is a
      successor to the current state, 'action' is the action
-     required to get there, and 'stepCost' is the incremental 
+     required to get there, and 'stepCost' is the incremental
      cost of expanding to that successor
      """
      util.raiseNotDefined()
@@ -50,12 +50,12 @@ class SearchProblem:
   def getCostOfActions(self, actions):
      """
       actions: A list of actions to take
- 
+
      This method returns the total cost of a particular sequence of actions.  The sequence must
      be composed of legal moves
      """
      util.raiseNotDefined()
-           
+
 
 def tinyMazeSearch(problem):
   """
@@ -71,20 +71,62 @@ def depthFirstSearch(problem):
   """
   Search the deepest nodes in the search tree first
   [2nd Edition: p 75, 3rd Edition: p 87]
-  
+
   Your search algorithm needs to return a list of actions that reaches
-  the goal.  Make sure to implement a graph search algorithm 
+  the goal.  Make sure to implement a graph search algorithm
   [2nd Edition: Fig. 3.18, 3rd Edition: Fig 3.7].
-  
+
   To get started, you might want to try some of these simple commands to
   understand the search problem that is being passed in:
-  
+
   print "Start:", problem.getStartState()
   print "Is the start a goal?", problem.isGoalState(problem.getStartState())
   print "Start's successors:", problem.getSuccessors(problem.getStartState())
   """
   "*** YOUR CODE HERE ***"
-  util.raiseNotDefined()
+  from game import Directions
+  start_position = problem.getStartState()
+  if problem.isGoalState(start_position):
+      return []
+
+  path = util.Stack()
+  explored = set()
+  explored.add(start_position)
+  frontier = util.Stack()
+  f_set = set()
+  for path_node in problem.getSuccessors(start_position):
+      frontier.push(path_node)
+      f_set.add(path_node[0]) # just the position
+
+  while frontier.isEmpty() != True:
+      current_node = frontier.pop()
+      f_set.remove(current_node[0])
+      current_position = current_node[0]
+
+      #restructure path so that we're tracking the way we got to this node
+      while path.isEmpty() != True:
+          path_head = path.pop()
+          path_successors = [tup[0] for tup in problem.getSuccessors(path_head[0])]
+          if current_position in path_successors:
+              path.push(path_head)
+              break
+      path.push(current_node)
+
+      if problem.isGoalState(current_position):
+          directions = []
+          # unwind the path into a direction list
+          while path.isEmpty() != True:
+              directions.insert(0,path.pop()[1])
+          return directions
+
+      explored.add(current_position)
+      successors = [node for node in problem.getSuccessors(current_position) if node[0] not in explored and node[0] not in f_set]
+      if len(successors) > 0:
+          for path_node in successors:
+              frontier.push(path_node)
+              f_set.add(path_node[0]) # just the position
+
+  return []
 
 def breadthFirstSearch(problem):
   """
@@ -93,7 +135,7 @@ def breadthFirstSearch(problem):
   """
   "*** YOUR CODE HERE ***"
   util.raiseNotDefined()
-      
+
 def uniformCostSearch(problem):
   "Search the node of least total cost first. "
   "*** YOUR CODE HERE ***"
@@ -110,8 +152,8 @@ def aStarSearch(problem, heuristic=nullHeuristic):
   "Search the node that has the lowest combined cost and heuristic first."
   "*** YOUR CODE HERE ***"
   util.raiseNotDefined()
-    
-  
+
+
 # Abbreviations
 bfs = breadthFirstSearch
 dfs = depthFirstSearch
